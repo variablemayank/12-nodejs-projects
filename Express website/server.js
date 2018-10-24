1a140cba7a39b541c27fdf818797bf991cd43714
variablemayank <mayank202020@rediffmail.com>
2018-10-24 14:03:47 +0530

    added auth in express website

--- Express website/server.js
+++ Express website/server.js
@@ -5,8 +5,6 @@ var nodemailer = require('nodemailer');
 
 var app = express();
 
-
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -26,5 +24,33 @@ app.get('/contact', function(req, res){
 	res.render('contact');
 });
 
-app.listen(3000); 
-console.log('Server is running on port '+3000);
+app.post('/contact/send', function(req, res){
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'thakurmayanksharma@gmail.com',
+			pass: 'qwerty@123456'
+		}
+	});
+
+	var mailOptions = {
+		from: 'Brad Traversy <techguyinfo@gmail.com>',
+		to: 'support@joomdigi.com',
+		subject: 'Website Submission',
+		text: 'You have a submission with the following details... Name: '+req.body.name+'Email: '+req.body.email+ 'Message: '+req.body.message,
+		html: '<p>You have a submission with the following details...</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
+	};
+
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			console.log(error);
+			res.redirect('/');
+		} else {
+			console.log('Message Sent: '+info.response);
+			res.redirect('/');
+		}
+	});
+});
+
+app.listen(3000);
+console.log('Server is running on port 3000...');
