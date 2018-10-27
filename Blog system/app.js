6f36af7403de0840a080f6006f5d1b063c59be85
variablemayank <mayank202020@rediffmail.com>
2018-10-27 19:15:05 +0530

    final commit

--- Blog system/app.js
+++ Blog system/app.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session = require('express-session');
-var multer = require('multer');
-var upload = multer({ dest: 'uploads/' })
-var expressValidator = require('express-validator');
-
+var session = require ('express-session');
+var multer = require ('multer');
+var upload = multer({dest:'./uploads'});  
+var expressValidator = require ('express-validator');
 var mongo = require('mongodb');
-var db = require('monk')('localhost/nodeblog');
+
+var db = require ('monk')('localhost/nodeblog');
+
+
 
 var routes = require('./routes/index');
 var posts = require('./routes/posts');
@@ -18,33 +20,21 @@ var categories = require('./routes/categories');
 
 var app = express();
 
-app.locals.moment = require('moment');
+app.locals.moment=require ('moment');
 
-app.locals.truncateText = function(text, length){
-  var truncatedText = text.substring(0, length);
-  return truncatedText;
-}
+//Express messages (Download this middleware from github 'Express messages')
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Express Session
-app.use(session({
-    secret: 'secret',
-    saveUninitialized: true,
-    resave: true
+app.use(session({  
+    secret:'secret',  
+    saveUninitialized: true,  
+    resave: true  
 }));
 
-// Express Validator
 app.use(expressValidator({
   errorFormatter: function(param, msg, value) {
       var namespace = param.split('.')
@@ -63,52 +53,61 @@ app.use(expressValidator({
 }));
 
 
-// Connect-Flash
-app.use(require('connect-flash')());
-app.use(function (req, res, next) {
-  res.locals.messages = require('express-messages')(req, res);
-  next();
-});
+app.locals.truncateText = function(text,length){
+  var truncatedText = text.substring(0,length);
+  return truncatedText;
+}
 
-// Make our db accessible to our router
+// Make db accesible to our router (Middleware)
 app.use(function(req,res,next){
     req.db = db;
     next();
-});
+  });
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/posts', posts);
 app.use('/categories', categories);
 
-// catch 404 and forward to error handler
+/// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handlers
+/// error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
