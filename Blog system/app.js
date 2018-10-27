4b2082a5db3b4ce4aaaa8645481976e9688b6577
variablemayank <mayank202020@rediffmail.com>
2018-10-27 18:34:11 +0530

    added editor

--- Blog system/app.js
+++ Blog system/app.js
@@ -1,77 +1,84 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var session = require('express-session');
-var moment = require('moment');
 var multer = require('multer');
 var upload = multer({ dest: 'uploads/' })
-
 var expressValidator = require('express-validator');
 
-
 var mongo = require('mongodb');
-
-var db = require('monk')('localhost/ndoeblog');
+var db = require('monk')('localhost/nodeblog');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var posts = require('./routes/posts');
+var categories = require('./routes/categories');
 
 var app = express();
+
+app.locals.moment = require('moment');
+
+app.locals.truncateText = function(text, length){
+  var truncatedText = text.substring(0, length);
+  return truncatedText;
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Expresss Session 
+// Express Session
 app.use(session({
-  secret:'secret',
-  saveUninitialized : true,
-  resave : true
-
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
 }));
 
-// Express Validator 
+// Express Validator
 app.use(expressValidator({
-  errorFormatter:function(params,msg,value){
-    var namespace = param.split('.')
-    ,root = namespace.shift()
-    ,formParam = root;
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
 
     while(namespace.length) {
-      formParam  +=  '[' + namespace.shift() + ']';
+      formParam += '[' + namespace.shift() + ']';
     }
     return {
-      param:formParam,
-      msg:msg,
-      value:value
-    }; 
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
   }
 }));
 
-//Connect- Flash
+
+// Connect-Flash
 app.use(require('connect-flash')());
-app.use(function(req,res,next){
-  res.locals.messages = require('express-messages')(req,res);
-  next();
-})
-
-
-// Make our db accessible to our router
-app.use(function(req,res,next){
-  req.db = db;
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
   next();
 });
 
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
 app.use('/', routes);
-app.use('/users', users);
+app.use('/posts', posts);
+app.use('/categories', categories);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -80,17 +87,19 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handlers
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
 
 // production error handler
 // no stacktraces leaked to user
@@ -101,5 +110,6 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
 
 module.exports = app;
